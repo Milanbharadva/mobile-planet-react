7ac8517fbce36a6f704375621c82c6b6e87a4fbd
milanbharadva1 <milanbharadva1@gmail.com>
2024-01-15 18:56:26 +0530

    form reset

--- src/components/admin/EditProduct.js
+++ src/components/admin/EditProduct.js
@@ -15,23 +15,27 @@ const EditProduct = ({ route }) => {
     }
   }, []);
   const notify = () => toast.success("Product updated sucessfully");
+  const formRef = useRef();
+
   const loadeddata = useFetch("product");
   const productforedit = loadeddata.loadeddata.filter(
     (item) => item.id == state.productid
   )[0];
-  const [formdata, setFormdata] = useState({
-    productname: "",
-    productprice: "",
-    productram: "",
-    productrom: "",
-    productcolor: "",
-    productcamera: "",
-    productbattery: "",
-    productdisplay: "",
-    productprocessor: "",
-    productimage: "",
-    categoryname: "",
-  });
+  const initialformdata = {
+    productname: productforedit && productforedit.productname,
+    productprice: productforedit && productforedit.productprice,
+    productram: productforedit && productforedit.productram,
+    productrom: productforedit && productforedit.productrom,
+    productcolor: productforedit && productforedit.productcolor,
+    productcamera: productforedit && productforedit.productcamera,
+    productbattery: productforedit && productforedit.productbattery,
+    productdisplay: productforedit && productforedit.productdisplay,
+    productprocessor: productforedit && productforedit.productprocessor,
+    productimage: productforedit && productforedit.productimage,
+    categoryname: productforedit && productforedit.categoryname,
+  };
+  console.log(initialformdata);
+  const [formdata, setFormdata] = useState(initialformdata);
   useEffect(() => {
     setFormdata(productforedit);
   }, [productforedit]);
@@ -50,63 +54,75 @@ const EditProduct = ({ route }) => {
   }
 
   return (
-    <>
+    <div className="bg-gray-200">
       <AdminNavbar />
-
-      <div>
-        <form
-          method="post"
-          onSubmit={validate}
-          className=" mt-5 flex flex-col justify-center items-center"
-        >
-          <div className="card-body">
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Product name </label>
-              <input
-                type="text"
-                name="productname"
-                placeholder="Enter Product name"
-                value={formdata && formdata.productname}
-                onChange={(e) => {
-                  handler(e);
-                }}
-                autoComplete="on"
-              />
-            </div>
-            <br />
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Product price </label>
-              <input
-                type="number"
-                name="productprice"
-                value={formdata && formdata.productprice}
-                placeholder="Enter Product price"
-                onChange={(e) => {
-                  handler(e);
-                }}
-              />
-            </div>
-            <br />
-
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Product RAM </label>
-              <input
-                type="number"
-                name="productram"
-                placeholder="Enter Product RAM"
-                value={formdata && formdata.productram}
-                onChange={(e) => {
-                  handler(e);
-                }}
-              />
-            </div>
-            <br />
-            <div className="form-group">
-              <div className="custom-file">
-                <label htmlFor="exampleInputFile">Product Image </label>
+      <div className="flex justify-center items-center">
+        <div className="bg-white p-8 rounded shadow-md w-[40vw]">
+          <h2 className="text-2xl font-semibold mb-6">Edit Product</h2>
+          <form method="post" ref={formRef} onSubmit={validate}>
+            <div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-600">
+                  Product name
+                </label>
+                <input
+                  type="text"
+                  name="productname"
+                  placeholder="Enter Product name"
+                  value={formdata && formdata.productname}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                  autoComplete="on"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product price
+                </label>
+                <input
+                  type="number"
+                  name="productprice"
+                  value={formdata && formdata.productprice}
+                  placeholder="Enter Product price"
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product RAM
+                </label>
+                <input
+                  type="number"
+                  name="productram"
+                  placeholder="Enter Product RAM"
+                  value={formdata && formdata.productram}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputFile"
+                >
+                  Product Image
+                </label>
                 <input
                   type="file"
-                  className="custom-file-input"
+                  className="mt-1 p-2 w-full border rounded-md"
                   id="exampleInputFile"
                   name="productimage"
                   onChange={(e) => {
@@ -115,107 +131,162 @@ const EditProduct = ({ route }) => {
                 />
                 {formdata && formdata.productimage}
               </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product ROM
+                </label>
+                <input
+                  type="number"
+                  name="productrom"
+                  placeholder="Enter Product ROM"
+                  value={formdata && formdata.productrom}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product Color
+                </label>
+                <input
+                  type="text"
+                  name="productcolor"
+                  placeholder="Enter Product Color"
+                  value={formdata && formdata.productcolor}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product Camera
+                </label>
+                <input
+                  type="text"
+                  name="productcamera"
+                  placeholder="Enter Product Camera"
+                  value={formdata && formdata.productcamera}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product Category
+                </label>
+                <input
+                  type="text"
+                  name="categoryname"
+                  placeholder="Enter Product Category"
+                  value={formdata && formdata.categoryname}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product Battery
+                </label>
+                <input
+                  type="text"
+                  name="productbattery"
+                  value={formdata && formdata.productbattery}
+                  placeholder="Enter Product Battery"
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product Display
+                </label>
+                <input
+                  type="text"
+                  name="productdisplay"
+                  value={formdata && formdata.productdisplay}
+                  placeholder="Enter Product Display"
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block text-sm font-medium text-gray-600"
+                  htmlFor="exampleInputEmail1"
+                >
+                  Product Processor
+                </label>
+                <input
+                  type="text"
+                  name="productprocessor"
+                  placeholder="Enter Product Processor"
+                  value={formdata && formdata.productprocessor}
+                  onChange={(e) => {
+                    handler(e);
+                  }}
+                  className="mt-1 p-2 w-full border rounded-md"
+                />
+              </div>
             </div>
-            <br />
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Product ROM </label>
-              <input
-                type="number"
-                name="productrom"
-                placeholder="Enter Product ROM"
-                value={formdata && formdata.productrom}
-                onChange={(e) => {
-                  handler(e);
-                }}
-              />
-              <br />
-              <br />
-              <label htmlFor="exampleInputEmail1">Product Color </label>
-              <input
-                type="text"
-                name="productcolor"
-                placeholder="Enter Product Color"
-                value={formdata && formdata.productcolor}
-                onChange={(e) => {
-                  handler(e);
-                }}
-              />
+
+            <div className="mb-4 flex gap-5">
+              <div>
+                <button
+                  type="submit"
+                  name="submit"
+                  value="add"
+                  className="btn btn-primary col-md-12 buttons"
+                >
+                  Submit
+                </button>
+              </div>
+              <div>
+                <button
+                  type="button"
+                  name="reset"
+                  value="reset"
+                  className="btn btn-primary col-md-12 buttons"
+                  onClick={() => setFormdata(initialformdata)}
+                >
+                  Reset
+                </button>
+              </div>
             </div>
-            <br />
-            <label htmlFor="exampleInputEmail1">Product Camera </label>
-            <input
-              type="text"
-              name="productcamera"
-              placeholder="Enter Product Camera"
-              value={formdata && formdata.productcamera}
-              onChange={(e) => {
-                handler(e);
-              }}
-            />
-            <br />
-            <br />
-            <label htmlFor="exampleInputEmail1">Product Category </label>
-            <input
-              type="text"
-              name="categoryname"
-              placeholder="Enter Product Category"
-              value={formdata && formdata.categoryname}
-              onChange={(e) => {
-                handler(e);
-              }}
-            />
-            <br />
-            <br />
-            <label htmlFor="exampleInputEmail1">Product Battery </label>
-            <input
-              type="text"
-              name="productbattery"
-              value={formdata && formdata.productbattery}
-              placeholder="Enter Product Battery"
-              onChange={(e) => {
-                handler(e);
-              }}
-            />
-            <br />
-            <br />
-            <label htmlFor="exampleInputEmail1">Product Display </label>
-            <input
-              type="text"
-              name="productdisplay"
-              value={formdata && formdata.productdisplay}
-              placeholder="Enter Product Display"
-              onChange={(e) => {
-                handler(e);
-              }}
-            />
-            <br />
-            <br />
-            <label htmlFor="exampleInputEmail1">Product Processor </label>
-            <input
-              type="text"
-              name="productprocessor"
-              placeholder="Enter Product Processor"
-              value={formdata && formdata.productprocessor}
-              onChange={(e) => {
-                handler(e);
-              }}
-            />
-          </div>
-          <br />
-          <div className="card-footer">
-            <button
-              type="submit"
-              name="submit"
-              value="add"
-              className="btn btn-primary col-md-12 buttons"
-            >
-              Submit
-            </button>
-          </div>
-          <br />
-        </form>
+          </form>
+        </div>
       </div>
-    </>
+      ;
+    </div>
   );
 };
 
